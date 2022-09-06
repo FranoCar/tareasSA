dad3b0c04d782cb63a0c979e1f017f18563623e5
Franco Carrión <franico_c@hotmail.com>
2022-09-06 17:44:31 -0400

    greedy aleatorio debuggeado

--- Tarea1/greedy_aleatorio.cpp
+++ Tarea1/greedy_aleatorio.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include <fstream>
+#include <random>
 #include <vector>
 #include <string>
 #include <map>
@@ -29,14 +30,27 @@ vector<string> getData(string dir){
 	if(input.is_open()){
 		string line;
 		while(getline(input,line)){
+			line.pop_back();
 			data.push_back(line);
 		}
 		input.close();
 	}
 	return data;
 }
+int hummingDist(string A, string B, int M){
+	int val = 0;
+	for(int i = 0; i < M; i++){
+		if (A[i] != B[i]){
+			val++;
+		}
+	}
+	return val;
+}
 
-string greedy_probabilista(vector<string> omega, int M, float th){
+string greedy_probabilista(vector<string> omega, int M, float th, float e){
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_real_distribution<> dis(0.0, 1.0);
 	string solucion;
 	for(int i = 0; i < M; i++){
 		map<char, int> frecuencia;
@@ -49,10 +63,22 @@ string greedy_probabilista(vector<string> omega, int M, float th){
 				frecuencia[c] = frecuencia[c] + 1;
 			}
 		}
+
+		char minChar = frecuencia.begin()->first;
+		vector<char> alfabeto;
 		for(auto const& pair : frecuencia){
-			cout << pair.first << "->" << pair.second << endl;
+			alfabeto.push_back(pair.first);
+			if(frecuencia[pair.first] < frecuencia[minChar])
+				minChar = pair.first;
 		}
-		break;
+
+		double p = dis(gen);
+		uniform_int_distribution<size_t> r_pick(0,alfabeto.size()-1);
+		if(p < e){
+			solucion.push_back(alfabeto[r_pick(gen)]);
+		}else{
+			solucion.push_back(minChar);
+		}
 	}
 	return solucion;
 }
@@ -68,10 +94,19 @@ int main(int argc, char const *argv[]){
 	omega = getData("dataset/" + instancia + ".txt");
 	//Longitud M de cada secuencia.
 	int M = omega[0].length();
+	string solucion = greedy_probabilista(omega,M,th,0.1);
 
-	string solucion = greedy_probabilista(omega,M,th);
+	cout << "Solución: ";
+	cout << solucion << endl;
 
-
+	int count = 0;
+	for(string s: omega){
+		int hd = hummingDist(s,solucion,M);
+		if( (float)hd/M >= th){
+			count++;
+		}
+	}
+	cout << count << endl;
 
 	return 0;
 }
