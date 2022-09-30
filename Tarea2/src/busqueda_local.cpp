4f43d995a5ea6af009d2fed867c681286325b886
Franco <franico_c@hotmail.com>
2022-09-30 19:57:32 -0300

    comentado y limpiado

--- Tarea2/src/busqueda_local.cpp
+++ Tarea2/src/busqueda_local.cpp
@@ -1,50 +1,52 @@
 #include "funciones.h"
 
-vector<string> vecindad(string sol){
+// Definición de la vecindad como todas las cadenas de caracteres similares
+// a el string solucion diferentes en una letra.
+vector<string> vecindad(string solucion){
     // Caracteres posibles, específicos a las instancias a usar.
 	vector<char> alfabeto{'A','C','G','T'};
-    vector<string> retorno;
+    vector<string> vecindad;
     
     //iterar en la solución
-     for (int i = 0; i < sol.length(); i++){
+     for (int i = 0; i < solucion.length(); i++){
         //Agregar los 3 vecinos posibles al arreglo
-        // Error 2: string vecino = sol; <- tiene que estar en el for de abajo.
         for(char letra : alfabeto){
-        	// Sólución 2 ------
-        	string vecino = sol;
-        	// -----------------
-            if(letra != sol[i]){
+        	string vecino = solucion;
+            if(letra != solucion[i]){
                 vecino[i] = letra;
             }
-            // Solución 1 ------------
-         	retorno.push_back(vecino);
-         	// -----------------------   
+         	vecindad.push_back(vecino);
         }
-        // ERROR 1: retorno.push_back(vecino); <- tiene que estar dentro del for de arriba
      }
-    return retorno;
+    return vecindad;
 }
 
-string siguiente_vecino(vector<string> omega, vector<string> vecindad, string sol, int M, float th){
-    int base = getValorObjetivo(omega,sol,M,th);
+string siguiente_vecino(vector<string> omega, vector<string> vecindad, string solucion, int M, float th){
+    // Valor objetivo del string solucion entregado
+    int base = getValorObjetivo(omega,solucion,M,th);
+    // Iteramos en la vecindad hasta encontrar un string mejor
+    // que la solución actual.
     for (string vecino : vecindad){
         if(getValorObjetivo(omega,vecino,M,th) > base)
             return vecino;
     }
-    return "";
+    return "";  //En el caso de no encontrar se retorna vacío.
 }
 
 string busqueda_local(vector<string> omega, string sol, int M, float th){
-    vector<string> vecinos = vecindad(sol); 				// Se genera la vecindad de la solución inicial.
-    string siguiente = siguiente_vecino(omega,vecinos,sol,M,th);	// Se obtiene la primera mejor opción en la vecindad.
-	string esta = sol; // Se le da valor inicial sol para evitar retornar vacío.
+    // Se genera la vecindad de la solución inicial.
+    vector<string> vecinos = vecindad(sol);
+    // Se obtiene la primera mejor opción en la vecindad.
+    string siguiente = siguiente_vecino(omega,vecinos,sol,M,th);	
+	string sol_busqueda = sol; // Se le da valor inicial sol para evitar retornar vacío.
+    // Mientras que existan vecinos mejores se sigue buscando
     while(!siguiente.empty()){
-        siguiente = siguiente_vecino(omega,vecinos,esta,M,th); //DE AQUI NO siguiente SALIENDO
+        siguiente = siguiente_vecino(omega,vecinos,sol_busqueda,M,th);
 
 		if(!siguiente.empty()){
-			esta = siguiente;
-			vecinos = vecindad(esta);
+			sol_busqueda = siguiente;
+			vecinos = vecindad(sol_busqueda);
 		}
     }
-    return esta;
+    return sol_busqueda;
 }
