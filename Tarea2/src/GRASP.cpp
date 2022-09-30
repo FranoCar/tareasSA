4a6edf9e1d5f237cee5627c42a10855c5f79af3a
Franco <franico_c@hotmail.com>
2022-09-30 20:20:37 -0300

    GRASP comentado y limpiado

--- Tarea2/src/GRASP.cpp
+++ Tarea2/src/GRASP.cpp
@@ -3,33 +3,33 @@
 using namespace std;
 
 string GRASP(vector<string> omega, int M, float e, float th, float time){
-    auto start = currentTime();
+    auto start = currentTime(); // Tiempo de inicio del algoritmo.
+    // Guardamos siempre la mejor solución encontrada.
     string solucion = "";
-    float tiempo_segundos = 0;
+    // Guardamos el tiempo para la solución final.
 	float tiempo_encontrado = 0;
 	int N = omega.size();
-    while(tiempo_segundos < time){
-        tiempo_segundos = timeDiff(start)/1000; //Error 5: *1000;
-        //Greedy aleatorizado
+    // Mientras que tiempo límite no es superado se continúa iterando.
+    while( (timeDiff(start)/1000) < time){
+        // Greedy aleatorizado
         string nueva = greedy_probabilista(omega,M,e);
-        //Busqueda local
+        // Busqueda local
         nueva = busqueda_local(omega,nueva,M,th);
-        if(solucion == "" || (getValorObjetivo(omega,solucion,M,th) < getValorObjetivo(omega,nueva,M,th)) ){
+
+        // Comparamos la nueva solución con la mejor actual.
+        int calidad_nueva = getValorObjetivo(omega,nueva,M,th);
+        if(solucion == "" || (calidad_nueva > getValorObjetivo(omega,solucion,M,th)) ){
+            // Si es mejor se reemplaza la solución y se avisa por consola.
             solucion = nueva;
-            if(solucion != ""){
-				tiempo_encontrado = timeDiff(start)/1000;
-                cout << "Nueva calidad: " << (float)getValorObjetivo(omega,nueva,M,th)/N << endl;
-                cout << "Encontrado en: " << tiempo_encontrado /* Error 5: 1000 */ << "(s)" << endl;
-				cout << "-----" << endl;
-            }
+			tiempo_encontrado = timeDiff(start)/1000;
+            cout << "Nueva calidad: " << (float)calidad_nueva/N << endl;
+            cout << "Encontrado en: " << tiempo_encontrado << "(s)" << endl;
+			cout << "-----" << endl;
         }
     }
 	cout << "Calidad final: " << (float)getValorObjetivo(omega,solucion,M,th)/N << endl;
 	cout << "Encontrado en: " << tiempo_encontrado << "(s)" << endl;
-	// Error 4: return 0; <- return 0??? en una función que retorna strings?
-	// Solución 4 --
 	return solucion;
-	// -------------
 
 }
 
