41606a6b42d93e466ca56ad5cef12bf1a3f5b9b5
Franco <franico_c@hotmail.com>
2022-11-03 16:31:00 -0300

    comentarios útiles

--- Tarea5/src/genetico.cpp
+++ Tarea5/src/genetico.cpp
@@ -1,1 +1,18 @@
 // Aquí implementaremos él algoritmo genético
+
+string AG(vector<string> omega, int M, float e, float th, float time){
+	// Generar Población
+	// población <- greedy_aleatorio o aleatorio completamente??
+	
+	// Evaluar Población
+	// Esto puede hacerse iterando toda la población y llamando a la función getValorObjetivo()
+	// Probablemente tendremos que usar 2 arreglos. uno para la población y otro para el puntaje.
+	// También se pueden hacer arreglos de pares.
+
+	// Bucle
+		// Selección
+		// Recombinar
+		// Evaluar Población
+		// Generar nueva población
+
+}
