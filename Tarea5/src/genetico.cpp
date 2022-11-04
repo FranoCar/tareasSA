68ca569b0bcee1aded5bea5d6803ac74bac28ca3
Franco <franico_c@hotmail.com>
2022-11-04 19:52:34 -0300

    arreglos

--- Tarea5/src/genetico.cpp
+++ Tarea5/src/genetico.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 string AG(vector<string> omega, int M, int n_agentes, float e, float th, float time){
 	auto start = currentTime(); // Tiempo de inicio del algoritmo.
+	float tiempo_encontrado = 0;
+	bool newrecord = false;
 	// Generar Población
 	vector<string> poblacion = gen_poblacion(n_agentes);
 	vector<int> fitness;
@@ -20,6 +22,11 @@ string AG(vector<string> omega, int M, int n_agentes, float e, float th, float t
 		}
 		fitness.push_back(curfit);
 	}
+	int N = omega.size();
+	tiempo_encontrado = timeDiff(start)/1000;
+	cout << "Nueva calidad: " << (float)fitbf/N << endl;
+    cout << "Encontrado en: " << tiempo_encontrado << "(s)" << endl;
+    cout << "-----" << endl;
 	while( (timeDiff(start)/1000) < time){
 		// Selección}
 		vector<string> padres = seleccion(poblacion,fitness);
@@ -32,7 +39,21 @@ string AG(vector<string> omega, int M, int n_agentes, float e, float th, float t
 		// Evaluar Población
 		fitness = vector<int>();
 		for(string agente : siggen){
-			fitness.push_back(getValorObjetivo(omega,agente,M,th));
+			int curfit = getValorObjetivo(omega,agente,M,th);
+			if(curfit > fitbf){
+				solbf = agente;
+				fitbf = curfit;
+				newrecord = true;				
+			}
+			fitness.push_back(curfit);
+		}
+		if(newrecord){
+			tiempo_encontrado = timeDiff(start)/1000;
+			cout << "Nueva calidad: " << (float)fitbf/N << endl;
+		    cout << "Encontrado en: " << tiempo_encontrado << "(s)" << endl;
+		    cout << "-----" << endl;
 		}
 	}
+	return solbf;
+
 }
--- Tarea5/src/reemp.cpp
+++ Tarea5/src/reemp.cpp
@@ -4,4 +4,5 @@ using namespace std;
 
 vector<string> reemplazo(vector<string> poblacion,vector<string> hijos){
 	cout << "Reemplazo no implementado, retornando..." << endl;
+	return vector<string>();
 }
