b7b930824554d32bccdbc858bc085202356bc12b
dlinette <dcerda2018@inf.udec.cl>
2022-11-05 13:43:15 -0400

    crossover

--- Tarea5/src/crossover.cpp
+++ Tarea5/src/crossover.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
-vector<string> crossover(vector<string> padres){
-	cout << "crossover no implementado, retornando..." << endl;
-	return vector<string>();
+vector<string> crossover(string padre1, string padre2){
+	random_device rd;
+	mt19937 gen(rd());
+	
+	uniform_int_distribution<size_t> p(0,100);
+
+	for(int i = 0; i < padre1.size(); i++){
+		if(p(gen) < 50){
+			char aux = padre1[i];
+			padre1[i] = padre2[i];
+			padre2[i] = aux; 
+		}
+	}
+
+	vector<string> hijos;
+	hijos.push_back(padre1);
+	hijos.push_back(padre2);
+
+	return hijos;
 }
--- Tarea5/src/genetico.hpp
+++ Tarea5/src/genetico.hpp
@@ -7,7 +7,7 @@ vector<string> gen_poblacion(int n_agentes, int M);
 
 vector<string> seleccion(vector<string> poblacion,vector<int> fitness);
 
-vector<string> crossover(vector<string> padres);
+vector<string> crossover(string padre1, string padre2);
 
 void mutar(vector<string> siggen);
 
